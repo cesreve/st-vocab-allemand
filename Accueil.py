568ed8c6a0858b01ea500e741f2aa77492dd7419
cesreve <cesreve@gmail.com>
2024-11-14 15:14:24 +0100

    Update Accueil.py
    Remoe technical aspects

--- Accueil.py
+++ Accueil.py
@@ -19,20 +19,4 @@ st.markdown("""
 - **Contrôler le nombre de mots :** Utilisez un curseur pour ajuster le nombre d'éléments de vocabulaire affichés.
 """)
 
-st.header("Comment l'utiliser ?")
-st.markdown("""
-1. **Installez Streamlit :** Si vous ne l'avez pas déjà fait, exécutez `pip install streamlit`.
-2. **Fichier de données :** Assurez-vous d'avoir un fichier CSV nommé `data.csv` dans le même répertoire que le script de l'application (`app.py`). 
-   Le fichier CSV doit comporter les colonnes suivantes :
-    - **Category :** La catégorie principale du vocabulaire.
-    - **Subcategory :** Une sous-catégorie plus spécifique au sein de la catégorie principale.
-    - **French :** Le mot ou l'expression en français.
-    - **Allemand :** La traduction allemande du mot ou de l'expression en français.
-3. **Exécutez l'application :** Ouvrez votre terminal, accédez au répertoire contenant `app.py` et exécutez `streamlit run app.py`.
-4. **Utilisez les filtres :** Sélectionnez les catégories et sous-catégories souhaitées dans la barre latérale pour filtrer la liste de vocabulaire.
-5. **Écoutez et apprenez :** Cliquez sur le bouton de lecture dans la colonne "Listen" pour entendre la prononciation allemande de chaque mot ou expression.
-6. **Personnalisez l'affichage :** Utilisez les cases à cocher pour afficher ou masquer les colonnes français et allemand. 
-   Ajustez le curseur pour contrôler le nombre de mots affichés.
-""")
-
 st.write("Commencez votre apprentissage dès aujourd'hui !")
